3bc9d56a1e5320b5a7c75049fda701cc64dc93d5
ingaSiu <ingasiud@gmail.com>
2025-02-07 08:22:55 +0200

    add id prop

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,7 +1,9 @@
 import { Button } from './ui/button';
 import Image from 'next/image';
+import Link from 'next/link';
 
 type Props = {
+  id: number;
   imgUrl: string;
   name: string;
   price: number;
@@ -9,31 +11,33 @@ type Props = {
   stock: number;
 };
 
-const ProductCard = ({ imgUrl, name, price, description, stock }: Props) => {
+const ProductCard = ({ id, imgUrl, name, price, description, stock }: Props) => {
   return (
-    <div className="startup-card">
-      <div className="flex flex-col">
-        <h3 className="text-26-semibold line-clamp-1">{name}</h3>
-        <div>
-          <p className="startup-card_desc">{description}</p>
-          <Image src={imgUrl} alt={name} width={140} height={164} className="startup-card_img" />
-        </div>
-        <div className="flex-between gap-3 mt-5">
-          <div className="flex items-center gap-2">
-            <p className="text-2xl ml-4 font-semibold">{price} $</p>
-            <p>
-              {stock <= 0 ? (
-                <span className="text-sm text-red-500"> • Out of Stock</span>
-              ) : (
-                <span className="text-sm text-green-500">• In Stock</span>
-              )}
-            </p>
+    <Link href={`products/${id}`}>
+      <div className="product-card">
+        <div className="flex flex-col">
+          <h3 className="text-26-semibold line-clamp-1">{name}</h3>
+          <div>
+            <p className="product-card_desc">{description}</p>
+            <Image src={imgUrl} alt={name} width={140} height={164} className="product-card_img" />
           </div>
+          <div className="flex-between gap-3 mt-5">
+            <div className="flex items-center gap-2">
+              <p className="text-2xl ml-4 font-semibold">{price} $</p>
+              <p>
+                {stock <= 0 ? (
+                  <span className="text-sm text-red-500"> • Out of Stock</span>
+                ) : (
+                  <span className="text-sm text-green-500">• In Stock</span>
+                )}
+              </p>
+            </div>
 
-          <Button className="startup-card_btn">Add to cart</Button>
+            <Button className="product-card_btn">Add to cart</Button>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
