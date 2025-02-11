73b0bffa5b84362a5c55d3212be1c16bcb432b19
ingaSiu <ingasiud@gmail.com>
2025-02-11 17:59:26 +0200

    add imagekit to nextjs config

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'placehold.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+        port: '',
+      },
     ],
   },
 };
