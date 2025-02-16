c5999bedecf14c61bdd7a70b1d9a9a1b9a4437a3
ingaSiu <ingasiud@gmail.com>
2025-02-16 19:21:20 +0200

    update index file

--- database/index.ts
+++ database/index.ts
@@ -1,6 +1,12 @@
-import config from '@/lib/config';
+import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 
-const sql = neon(config.env.databaseUrl!);
+config({ path: '.env.local' });
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not defined. Check your .env file.');
+}
+
+const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle({ client: sql });
