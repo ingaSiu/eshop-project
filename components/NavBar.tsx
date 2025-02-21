e3325ec10f358d4e942ca746c9085ba25a7b7eb2
ingaSiu <ingasiud@gmail.com>
2025-02-21 15:14:53 +0200

    add cart to nav, change font size

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,13 +1,15 @@
+import Cart from './Cart';
 import Link from 'next/link';
-import React from 'react';
 
 const NavBar = () => {
   return (
     <header className="px-5 py-3 bg-white shadow-sm ">
       <nav className="flex justify-between items-center">
         <Link href="/">
-          <h1 className="text-primary text-3xl uppercase font-bold font-bebas-neue">Nekotastic Shop 😼</h1>
+          <h1 className="text-primary text-xl md:text-3xl uppercase font-bold font-bebas-neue">Nekotastic Shop 😼</h1>
         </Link>
+
+        <Cart />
       </nav>
     </header>
   );
