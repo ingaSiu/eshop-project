9502468e8db887ca3afa40d57357f39f2ba8ada5
ingaSiu <ingasiud@gmail.com>
2025-02-21 15:36:29 +0200

    calculate total, add separators, move removal all btn

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -18,9 +18,8 @@ import { Separator } from '@radix-ui/react-separator';
 import { ShoppingCart } from 'lucide-react';
 import { useCart } from '@/context/CartProvider';
 
-// TODO: use countTotalPrice
 const Cart = () => {
-  const { items: cartItems, removeFromCart, clearCart } = useCart();
+  const { items: cartItems, removeFromCart, clearCart, countTotalPrice } = useCart();
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -29,17 +28,15 @@ const Cart = () => {
       <SheetContent side="right">
         <SheetHeader>
           <SheetTitle className="text-white">Meowjestic Cart 😻</SheetTitle>
-          <Button className="bg-inherit w-fit text-white font-semibold" onClick={clearCart}>
-            Remove all Products
-          </Button>
+          <Separator className="w-full h-0.5 bg-gray-200 mb-2" />
         </SheetHeader>
 
         {cartItems.map((cartItem) => {
           return (
             <SheetDescription key={cartItem.product.id} className="">
-              <div className="flex space-x-4 mb-2 text-white">
+              <div className="flex space-x-4 mb-4 text-white ">
                 <Image
-                  className="object-cover rounded-md"
+                  className="object-cover rounded-md hidden md:block"
                   src={cartItem.product.imageUrl}
                   alt={cartItem.product.title}
                   width={60}
@@ -47,7 +44,7 @@ const Cart = () => {
                 />
                 <div>
                   <div className="flex-1">
-                    <h3 className="font-semibold">{cartItem.product.title}</h3>
+                    <h3 className="font-semibold text-center">{cartItem.product.title}</h3>
 
                     <div className="flex justify-between pb-4">
                       <div className="flex text-black-300 text-sm font-semibold space-x-1">
@@ -72,12 +69,23 @@ const Cart = () => {
           );
         })}
 
-        <Separator className="my-4 bg-orange-600" />
+        <Separator className="w-full h-0.5 bg-gray-200 mb-2" />
 
-        <SheetDescription></SheetDescription>
+        <Button className="bg-inherit w-fit text-white font-semibold" onClick={clearCart}>
+          Remove all Products
+        </Button>
+
+        <SheetDescription>
+          <div className="text-white">
+            <h1 className="text-2xl mb-2">Total:</h1>
+            <p className="font-semibold text-xl mb-4">
+              <span className="text-gray-400 ">The total of your cart is:</span> €{countTotalPrice()}
+            </p>
+          </div>
+        </SheetDescription>
 
         <SheetClose asChild>
-          <Button type="submit" className="w-full" asChild>
+          <Button type="submit" className="w-full text-white font-semibold" asChild>
             <Link href="/">CHECKOUT</Link>
           </Button>
         </SheetClose>
