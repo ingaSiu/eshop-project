ff78ba70f7909b9b10a55cf3b83e78b186a7ed16
ingaSiu <ingasiud@gmail.com>
2025-03-03 10:03:16 +0200

    modify query for filter functionality

--- lib/queries/getSortedProducts.ts
+++ lib/queries/getSortedProducts.ts
@@ -1,9 +1,9 @@
-import { asc, desc } from 'drizzle-orm';
+import { CATEGORY_ENUM, products } from '@/database/schema';
+import { asc, desc, inArray } from 'drizzle-orm';
 
 import { db } from '@/database';
-import { products } from '@/database/schema';
 
-export async function getSortedProducts(sort?: string) {
+export async function getSortedProducts(sort?: string, categories?: string[]) {
   let orderByCondition;
 
   switch (sort) {
@@ -23,5 +23,16 @@ export async function getSortedProducts(sort?: string) {
       orderByCondition = desc(products.createdAt);
   }
 
-  return await db.select().from(products).orderBy(orderByCondition);
+  const validCategories = categories?.filter((cat): cat is (typeof CATEGORY_ENUM.enumValues)[number] =>
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    CATEGORY_ENUM.enumValues.includes(cat as any),
+  );
+
+  const query = db
+    .select()
+    .from(products)
+    .where(validCategories && validCategories.length > 0 ? inArray(products.category, validCategories) : undefined)
+    .orderBy(orderByCondition);
+
+  return await query;
 }
